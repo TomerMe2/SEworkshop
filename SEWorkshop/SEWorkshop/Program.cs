e3258218825ea605afb29d98aad80a52f1266e4b
Tomer Laor <tomer.laor1@gmail.com>
2020-04-07 15:36:33 +0300

    Init entities

---
+++ SEWorkshop/SEWorkshop/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SEWorkshop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
