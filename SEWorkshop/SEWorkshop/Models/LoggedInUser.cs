c3b5a6b0c87591088d88c148d3fac95ad41b1271
Ravid Levy <ravidle@post.bgu.ac.il>
2020-04-20 16:21:58 +0300

    Compiling Fixes

--- SEWorkshop/SEWorkshop/Models/LoggedInUser.cs
+++ SEWorkshop/SEWorkshop/Models/LoggedInUser.cs
@@ -6,11 +6,11 @@ namespace SEWorkshop
 {
     public class LoggedInUser : User
     {
-        public IList<Store> Owns { get; private set; }
-        public IList<Store> Manages { get; private set; }
+        public ICollection<Store> Owns { get; private set; }
+        public ICollection<Store> Manages { get; private set; }
         public IList<Review> Reviews { get; private set; }
         public IList<Message> Messages { get; private set; }
-        public IList<Purchase> Purchase_Histroy{get; private set;}
+        public IList<Purchase> PurchaseHistory{get; private set;}
         public string Username {get; private set;}
         public string Password {get; private set;} 
 
@@ -22,70 +22,7 @@ namespace SEWorkshop
             Manages = new List<Store>();
             Reviews = new List<Review>();
             Messages = new List<Message>();
-            Purchase_Histroy= new List<Purchase>();
-        }
-
-        public void Logout(){
-            Facades.LoggedInUserFacade.getInstance().Logout();
-        }
-
-        public void OpenStore(string store_name){
-            Store new_store= new Store(Facades.LoggedInUserFacade.getInstance().LoggedInUser, store_name);
-            Owns.Add(new_store);
-            Console.WriteLine("store: "+ store_name + " has opened\n");
-
-        } 
-
-        public Boolean PurchaseHistoryContainsProduct(Product pro){
-            foreach(Purchase p in Purchase_Histroy){
-                foreach(Cart c in p){
-                    foreach(Product prod in c){
-                        if(prod.Name ==pro.Name && pro.Store==prod.Store){
-                            return true;
-                        }
-                    }
-                }
-            }
-            return false;
-        }
-
-        public void WriteReview(string review, Product p){
-            if (PurchaseHistoryContainsProduct(p) && review != ""){
-                Review new_review= new Review(review, p);
-                Reviews.Add(this.RegisteredUser ,p,review);
-                 Console.WriteLine("Review has been sent\n");
-            }
-            else {
-                Console.WriteLine("Unable to send review\n");
-            }
-
-        }
-
-        public void SendMessage(string message, Store store){
-            if(message!=""){
-
-                Messages new_message= new Message(store,this.RegisteredUser,Messages.Last() ,null);
-                if(Messages.Last() != null){
-                    Messages.Last().Next()= new_message;
-                }
-                Messages.Add(new_message);
-                 Console.WriteLine("Message has been sent\n");
-            }
-            else{
-                 Console.WriteLine("Unable to send message\n");
-            }
-        }
-
-        public void ViewPurchaseHistory(){
-            foreach(Purchase p in Purchase_Histroy){
-               foreach(Cart c in p){
-                   foreach(Basket b in c){
-                       foreach(Product pr in b){
-                        Console.WriteLine("Product name: "+ pr.product_name+ " Store: "+ pr.Store+ " Policy: "+ pr.Policy +"\n");
-                       }
-                   }
-               }
-            }
+            PurchaseHistory= new List<Purchase>();
         }
     }
 }
--- SEWorkshop/SEWorkshop/ServiceLayer/AdministratorUI.cs
+++ SEWorkshop/SEWorkshop/ServiceLayer/AdministratorUI.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace SEWorkshop.ServiceLayer
 {
-    class AdministratorUI : LoggedInUser
+    class AdministratorUI : UserUI
     {
     }
 }
