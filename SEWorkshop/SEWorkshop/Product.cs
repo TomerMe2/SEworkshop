d4e11dee214def4ec34e24ee75c4db04c05a6e52
Tomer Laor <tomer.laor1@gmail.com>
2020-04-21 17:33:23 +0300

    Removed policy from product

--- SEWorkshop/SEWorkshop/Product.cs
+++ SEWorkshop/SEWorkshop/Product.cs
@@ -6,16 +6,14 @@ namespace SEWorkshop
 {
     class Product
     {
-        public Policy Policy { get; private set; }
         public Store Store { get; private set; }
         public string Name { get; set; }
         public string Description { get; set; }
         public string Category { get; set; }
         public double Price { get; set; }
 
-        public Product(Policy policy, Store store, string name, string description, string category, double price)
+        public Product(Store store, string name, string description, string category, double price)
         {
-            Policy = policy;
             Store = store;
             Name = name;
             Description = description;
