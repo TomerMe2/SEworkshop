457597af49be56382a963ef4ed084c47948d5551
Tomer Laor <tomer.laor1@gmail.com>
2020-04-27 01:18:07 +0300

    Add logs calls

--- SEWorkshop/SEWorkshop/Facades/StoreFacade.cs
+++ SEWorkshop/SEWorkshop/Facades/StoreFacade.cs
@@ -35,24 +35,26 @@ namespace SEWorkshop.Facades
         /// <returns>The created store</returns>
         public Store CreateStore(LoggedInUser owner, string storeName)
         {
+            log.Info(string.Format("CreateStore was invoked with userName {0}, storeName {1}", owner.Username, storeName));
             Func<Store, bool> StoresWithThisNamePredicate = store => store.Name.Equals(storeName);
             ICollection<Store> StoresWithTheSameName = SearchStore(StoresWithThisNamePredicate);
             if (StoresWithTheSameName.Count > 0)
             {
-                log.Info(String.Format("User {0} tried to create a store with name {1} and it's already exist",
+                log.Info(string.Format("User {0} tried to create a store with name {1} and it's already exist",
                     owner.Username, storeName));
                 throw new StoreWithThisNameAlreadyExistsException();
             }
             Store newStore = new Store(owner, storeName);
             Stores.Add(newStore);
             owner.Owns.Add(newStore);
-            log.Info(String.Format("User {0} created a store with name {1}",
+            log.Info(string.Format("User {0} created a store with name {1}",
                     owner.Username, storeName));
             return newStore;
         }
 
         public ICollection<Store> BrowseStores()
         {
+            log.Info("BrowseStore was invoked");
             return (from store in Stores
                     where store.IsOpen
                     select store).ToList();
@@ -63,6 +65,7 @@ namespace SEWorkshop.Facades
         /// </summary>
         public ICollection<Store> SearchStore(Func<Store, bool> pred)
         {
+            log.Info("SearchStore was invoked");
             return (from store in BrowseStores()
                     where pred(store)
                     select store).ToList();
@@ -70,6 +73,7 @@ namespace SEWorkshop.Facades
 
         public bool IsProductExists(Product product)
         {
+            log.Info("IsProductExists was invoked with product name of {0} in stoer name of {1}", product.Name, product.Store.Name);
             return (from prod in AllActiveProducts()
                     where prod == product
                     select prod).Any();
@@ -77,6 +81,7 @@ namespace SEWorkshop.Facades
 
         public IEnumerable<Product> AllActiveProducts()
         {
+            log.Info("AllActiveProduct was invoked");
             return BrowseStores().Aggregate(Enumerable.Empty<Product>(), (acc, store) => Enumerable.Concat(acc, store.Products));
         }
 
@@ -85,6 +90,7 @@ namespace SEWorkshop.Facades
         /// </summary>
         public ICollection<Product> SearchProducts(Func<Product, bool> pred)
         {
+            log.Info("SearchProducts was invoked");
             return (from product in AllActiveProducts()
                     where pred(product)
                     select product).ToList();
@@ -92,6 +98,7 @@ namespace SEWorkshop.Facades
 
         public ICollection<Product> FilterProducts(ICollection<Product> products, Func<Product, bool> pred)
         {
+            log.Info("FilterProducts was invoked");
             if (products.Count == 0)
             {
                 log.Info("Attemp to filter an empty collection");
