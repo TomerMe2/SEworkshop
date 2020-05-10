43133024aa76c0824ddecb740dd33c358fc0f717
Tomer Laor <tomer.laor1@gmail.com>
2020-05-11 02:48:19 +0300

    Fixed filter

--- SEWorkshop/Website/Pages/SearchResult.cshtml.cs
+++ SEWorkshop/Website/Pages/SearchResult.cshtml.cs
@@ -15,49 +15,45 @@ namespace Website.Pages
         public string RequestedSearchString { get; private set; }
         public string SearchCategory { get; private set; }
         public string ActualSearchString { get; private set; }
-        public int PriceMin { get; private set; }
-        public int PriceMax { get; private set; }
-        public string CategoryFilter { get; private set; }
+        public int? PriceMin { get; private set; }
+        public int? PriceMax { get; private set; }
+        public string? CategoryFilter { get; private set; }
         public string Error { get; private set; }
-        public bool Filter { get; private set; }
-        public static string remember { get; private set; }
         public IEnumerable<DataProduct> SearchResult { get; private set; }
-        public IEnumerable<DataProduct> FilteredSearchResult { get; private set; }
-        public ICollection<DataProduct> CurrSearchResult { get; private set; }
 
         public SearchResultModel(IUserManager userManager)
         {
-            Console.WriteLine("here");
             UserManager = userManager;
+            SearchResult = new List<DataProduct>();
             RequestedSearchString = "";
             ActualSearchString = "";
             SearchCategory = "";
-            PriceMin = -1;
-            PriceMax = -1;
-            CategoryFilter = "";
             Error = "";
-            Filter = false;
-            SearchResult = new List<DataProduct>();
-            FilteredSearchResult = new List<DataProduct>();
-            try
-            {
-                userManager.Register("wello", "1234");
-                userManager.Login("wello", "1234");
-                userManager.OpenStore("nini");
-                userManager.AddProduct("nini", "prod1", "some description1", "cat1", 123.7, 7);
-                userManager.AddProduct("nini", "prod3", "some description1", "cat1", 250, 7);
-                userManager.AddProduct("nini", "prod2", "some LONGGGGGGGGGGGGGGGGGGGGGGGGG descriptionnnnnnnnnnnnnnn2", "cat2", 999999, 1);
-            }
-            catch { }
         }
 
-        public void OnPost(string searchText, string searchCategory, string minPriceText, string maxPriceText, string filterCategoryText)
-        { 
-            Filter = false;
-            Error = "";
-            PriceMin = -1;
-            PriceMax = -1;
-            FilteredSearchResult = new List<DataProduct>();
+        public void OnPost(string searchText, string searchCategory, string minPriceText, string maxPriceText, string categoryFilterText)
+        {
+            if (!string.IsNullOrEmpty(categoryFilterText))
+            {
+                CategoryFilter = categoryFilterText;
+            }
+            try
+            {
+                if (!string.IsNullOrEmpty(minPriceText))
+                {
+                    PriceMin = int.Parse(minPriceText);
+                }
+                if (!string.IsNullOrEmpty(maxPriceText))
+                {
+                    PriceMax = int.Parse(maxPriceText);
+                }
+            }
+            catch
+            {
+                Error = "Min price or max price contained non numbers";
+            }
+
+
             RequestedSearchString = searchText;
             SearchCategory = searchCategory;
             string searchString = RequestedSearchString;
@@ -68,9 +64,27 @@ namespace Website.Pages
                 "product_keywords" => UserManager.SearchProductsByKeywords(ref searchString),
                 _ => new List<DataProduct>()
             };
-            remember = searchString;
-            Deep_Copy();
+            ActualSearchString = searchString;
+            FilterResults();
         }
+
+        private void FilterResults()
+        {
+            if(PriceMax != null)
+            {
+                SearchResult = UserManager.FilterProducts(SearchResult.ToList(), prod => prod.Price <= PriceMax);
+            }
+            if (PriceMin != null)
+            {
+                SearchResult = UserManager.FilterProducts(SearchResult.ToList(), prod => prod.Price >= PriceMin);
+            }
+            if (CategoryFilter != null)
+            {
+                SearchResult = UserManager.FilterProducts(SearchResult.ToList(), prod => prod.Category.Equals(CategoryFilter));
+            }
+        }
+
+        /*
 
         public void OnPostEdit(string minPriceText, string maxPriceText, string categoryFilterText)
         {
@@ -105,14 +119,6 @@ namespace Website.Pages
                 Error = "Invalid Price Range";
             }
         }
-
-        private void Deep_Copy()
-        {
-            CurrSearchResult = new List<DataProduct>();
-            foreach (DataProduct dp in SearchResult)
-            {
-                CurrSearchResult.Add(dp);
-            }
-        }
+        */
     }
 }
