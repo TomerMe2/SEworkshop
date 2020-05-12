c5786cd076a71d2b81b0ec5036adfff2055044fe
Amit Vigdor Bart <amitvigdorbart@gmail.com>
2020-05-12 12:46:04 +0300

    added search store functionality

---
+++ SEWorkshop/Website/Pages/SearchStore.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SEWorkshop.DataModels;
+using SEWorkshop.ServiceLayer;
+
+namespace Website.Pages
+{
+    public class SearchStoreModel : PageModel
+    {
+        private IUserManager UserManager;
+        
+        [BindProperty(SupportsGet = true)]
+        public string StoreName { get; set; }
+
+        public DataStore Store { get; private set; }
+        
+        [BindProperty(SupportsGet = true)]
+        public string Error { get; set; }
+
+        public SearchStoreModel(IUserManager userManager)
+        {
+            UserManager = userManager;
+            StoreName = "";
+            Error = "";
+        }
+        
+        public void OnGet()
+        {
+            Error = "";
+        }
+
+        public IActionResult OnPost()
+        {
+            try
+            {
+                Store = UserManager.SearchStore(StoreName);
+            }
+            catch (Exception)
+            {
+                Error = "No Results";
+            }
+            return RedirectToPage("./Store", new { storeName = StoreName });
+        }
+    }
+}
