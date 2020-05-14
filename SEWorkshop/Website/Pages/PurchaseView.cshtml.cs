ba34a28a12decc3043c299629e1493420cbe51ce
Roy Poliansky <roypoli@post.bgu.ac.il>
2020-05-14 14:37:46 +0300

    done gui for writing reviews

--- SEWorkshop/Website/Pages/PurchaseView.cshtml.cs
+++ SEWorkshop/Website/Pages/PurchaseView.cshtml.cs
@@ -5,16 +5,22 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using SEWorkshop.DataModels;
+using SEWorkshop.ServiceLayer;
 
 namespace Website.Pages
 {
     public class PurchaseViewModel : PageModel
     {
+        public IUserManager UserManager { get; }
         public DataPurchase Purchase { get; private set; }
+        public bool CanReview { get; private set; }
         public double TotalPrice { get; private set; }
-        public PurchaseViewModel(DataPurchase purchase)
+        public string Purchaser { get; private set; }
+        public PurchaseViewModel(IUserManager userManager, DataPurchase purchase, bool canReview)
         {
+            UserManager = userManager;
             Purchase = purchase;
+            CanReview = canReview;
             TotalPrice = 0;
             foreach(var pair in Purchase.Basket.Products)
             {
--- SEWorkshop/Website/Pages/UserPage.cshtml.cs
+++ SEWorkshop/Website/Pages/UserPage.cshtml.cs
@@ -12,16 +12,43 @@ namespace Website.Pages
 {
     public class UserPageModel : PageModel
     {
-        private IUserManager UserManager { get; }
+        public IUserManager UserManager { get; }
         public IEnumerable<DataPurchase> purchases { get; private set; }
         public UserPageModel(IUserManager userManager)
         {
             UserManager = userManager;
+            purchases = new List<DataPurchase>();
             
         }
         public void OnGet()
         {
+            try
+            {
+                Address adr1 = new Address("rehovot", "hertzel", "206");
+                Address adr2 = new Address("beersheva", "hertzel", "206");
+                string sid = HttpContext.Session.Id;
+                UserManager.Register(sid, "wello", "1234");
+                UserManager.Login(sid, "wello", "1234");
+                UserManager.OpenStore(sid, "nini");
+                UserManager.AddProduct(sid, "nini", "prod1", "some description1", "cat1", 123.7, 7);
+                UserManager.AddProduct(sid, "nini", "prod2", "some LONGGGGGGGGGGGGGGGGGGGGGGGGG descriptionnnnnnnnnnnnnnn2", "cat2", 999999, 1);
+                UserManager.AddProductToCart(sid, "nini", "prod1", 6);
+                UserManager.AddProductToCart(sid, "nini", "prod2", 1);
+                UserManager.Purchase(sid, UserManager.MyCart(sid).First(), "12345", adr1);
+                UserManager.OpenStore(sid, "bibi");
+                UserManager.AddProduct(sid, "bibi", "brod1", "bsome description1", "cat1", 150, 7);
+                UserManager.AddProduct(sid, "bibi", "brod2", "bsome LONGGGGGGGGGGGGGGGGGGGGGGGGG descriptionnnnnnnnnnnnnnn2", "bcat2", 873, 1);
+                UserManager.AddProductToCart(sid, "bibi", "brod1", 3);
+                UserManager.Purchase(sid, UserManager.MyCart(sid).First(), "123456", adr2);
+            }
+            catch { }
             purchases = UserManager.PurchaseHistory(HttpContext.Session.Id);
         }
+        public void OnPost(string content, string storeName, string productName)
+        {
+            string sid = HttpContext.Session.Id;
+            UserManager.WriteReview(sid, storeName, productName, content);
+        }
+
     }
 }
