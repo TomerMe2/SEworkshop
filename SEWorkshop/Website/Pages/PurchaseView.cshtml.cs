f0ab916bb2f16ffe269e3edd0180cc6bc984e9b2
Roy Poliansky <roypoli@post.bgu.ac.il>
2020-05-14 12:50:39 +0300

    Added Single purchase view and user purchase history view

---
+++ SEWorkshop/Website/Pages/PurchaseView.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SEWorkshop.DataModels;
+
+namespace Website.Pages
+{
+    public class PurchaseViewModel : PageModel
+    {
+        public DataPurchase Purchase { get; private set; }
+        public double TotalPrice { get; private set; }
+        public PurchaseViewModel(DataPurchase purchase)
+        {
+            Purchase = purchase;
+            TotalPrice = 0;
+            foreach(var pair in Purchase.Basket.Products)
+            {
+                TotalPrice += pair.Item1.Price * pair.Item2;
+            }
+        }
+        public void OnGet()
+        {
+
+        }
+    }
+}
---
+++ SEWorkshop/Website/Pages/UserPage.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SEWorkshop;
+using SEWorkshop.DataModels;
+using SEWorkshop.ServiceLayer;
+
+namespace Website.Pages
+{
+    public class UserPageModel : PageModel
+    {
+        private IUserManager UserManager { get; }
+        public IEnumerable<DataPurchase> purchases { get; private set; }
+        public UserPageModel(IUserManager userManager)
+        {
+            UserManager = userManager;
+            
+        }
+        public void OnGet()
+        {
+            purchases = UserManager.PurchaseHistory(HttpContext.Session.Id);
+        }
+    }
+}
