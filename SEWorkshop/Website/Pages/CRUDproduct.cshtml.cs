edc64d24eb6035d45e40d9f71bd7349ad988a07c
lynn <nabulsylynn@gmail.com>
2020-05-14 19:21:00 +0300

    create a product

--- SEWorkshop/Website/Pages/CRUDproduct.cshtml.cs
+++ SEWorkshop/Website/Pages/CRUDproduct.cshtml.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using SEWorkshop.ServiceLayer;
 using SEWorkshop.DataModels;
 using SEWorkshop.Exceptions;
+using System.Security.Cryptography;
 
 namespace Website.Pages.Shared
 {
@@ -16,18 +17,60 @@ namespace Website.Pages.Shared
         private IUserManager UserManager { get; }
         public DataProduct? Product { get; private set; }
         public string ProductName { get; private set; }
+        public string ProductCategory { get; private set; }
+        public string ProductDescription { get; private set; }
+        public double ProductPrice { get; private set; }
+        public int ProductQuantity { get; private set; }
+        public string StoreName { get; private set; }
         public string ErrorMsg { get; private set; }
 
         public CRUDproductModel(IUserManager userManager) {
             UserManager = userManager;
             ProductName = "";
-
+            ProductCategory = "";
+            ProductDescription = "";
+            ProductPrice = 0.00;
+            ProductQuantity = 0;
             ErrorMsg = "";
 
         }
         public void OnGet()
         {
+            ErrorMsg = "";
+        }
+        public IActionResult OnPost(string ProductName, string StoreName,  string ProductDescription, string ProductCategory, string ProductPrice, string ProductQuantity)
+        {
+            try
+            {
+                UserManager.AddProduct(HttpContext.Session.Id, StoreName, ProductName, ProductDescription, ProductCategory, double.Parse(ProductPrice), int.Parse(ProductQuantity));
+            }
+            catch (UserHasNoPermissionException e)
+            {
+                ErrorMsg = e.ToString();
+            }
+            catch (ProductAlreadyExistException e)
+            {
+                ErrorMsg = e.ToString();
+            }
+            catch (NegativeQuantityException e)
+            {
+                ErrorMsg = e.ToString();
+            }
+            catch (StoreNotInTradingSystemException e)
+            {
+                ErrorMsg = e.ToString();
+            }
+         
+            this.ProductName = ProductName;
+            this.StoreName = StoreName;
+            this.ProductCategory = ProductCategory;
+            this.ProductDescription = ProductDescription;
+            this.ProductPrice =double.Parse(ProductPrice);
+            this.ProductQuantity =int.Parse(ProductQuantity);
+
+            return RedirectToPage("./Store", new {storeName= StoreName});
 
         }
+
     }
 }
