0e5b848feaf82763f47c6713e59db4c5bbfce8ab
lynn <nabulsylynn@gmail.com>
2020-05-14 16:59:20 +0300

    create CRUD for product

---
+++ SEWorkshop/Website/Pages/CRUDproduct.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SEWorkshop.ServiceLayer;
+using SEWorkshop.DataModels;
+using SEWorkshop.Exceptions;
+
+namespace Website.Pages.Shared
+{
+
+    public class CRUDproductModel : PageModel
+    {
+        private IUserManager UserManager { get; }
+        public DataProduct? Product { get; private set; }
+        public string ProductName { get; private set; }
+        public string ErrorMsg { get; private set; }
+
+        public CRUDproductModel(IUserManager userManager) {
+            UserManager = userManager;
+            ProductName = "";
+
+            ErrorMsg = "";
+
+        }
+        public void OnGet()
+        {
+
+        }
+    }
+}
