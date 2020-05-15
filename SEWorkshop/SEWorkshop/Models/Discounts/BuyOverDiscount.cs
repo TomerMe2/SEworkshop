abc318e71eed813dfb9554c2162b2e4829af3f29
lynn <nabulsylynn@gmail.com>
2020-05-15 21:22:54 +0300

    conditional discount on basket was added

---
+++ SEWorkshop/SEWorkshop/Models/Discounts/BuyOverDiscount.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEWorkshop.Models.Discounts
+{
+    public abstract class BuyOverDiscount : ConditionalDiscount
+    {
+        public Store Store { get; set; }
+        public int MinSum { get; set; }
+        public int OffSum { get; set; }
+
+        public BuyOverDiscount(Store store, int minSum, int offSum, double percentage, DateTime deadline, Product product, User user) : base(percentage, deadline, product, store, user)
+        {
+            Store = store;
+            MinSum = minSum;
+            OffSum = offSum;
+        }
+
+      protected override double ApplyDiscount(Basket basket)
+        {
+            double sumBasket = 0;
+            foreach (var product in basket.Products)
+            {
+                sumBasket = sumBasket + (product.Item1.Price * product.Item2);
+            }
+            if(sumBasket>= MinSum)
+            {
+                return sumBasket * (1 - OffSum/ 100);
+
+            }
+            return 0;
+        }
+
+    }
+
+}
