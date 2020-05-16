daae8e21f46695285f7ae7d574ba1fa2189f92c5
lynn <nabulsylynn@gmail.com>
2020-05-16 12:08:20 +0300

    updated all conditional discounts to the branch

---
+++ SEWorkshop/SEWorkshop/Models/Discounts/BuyOverDiscount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SEWorkshop.Models.Discounts
+{
+    public abstract class BuyOverDiscount : ConditionalDiscount
+    {
+        public Store Store { get; set; }
+        public int MinSum { get; set; }
+        public int OffSum { get; set; }
+
+        public BuyOverDiscount(Store store, int minSum, int offSum, double percentage, DateTime deadline, Product product) : base(percentage, deadline, product, store)
+        {
+            Store = store;
+            MinSum = minSum;
+            OffSum = offSum;
+        }
+        protected override double ApplyDiscount(Basket basket)
+        {
+            double sumBasket = 0;
+            foreach (var product in basket.Products)
+            {
+                sumBasket = sumBasket + (product.Item1.Price * product.Item2);
+            }
+            if (sumBasket >= MinSum)
+            {
+                return sumBasket * (1 - OffSum / 100);
+
+            }
+            return 0;
+        }
+    }
+}
---
+++ SEWorkshop/SEWorkshop/Models/Discounts/ConditionalDiscount.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework.Interfaces;
+using SEWorkshop.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SEWorkshop.Models.Discounts
+{
+    public abstract class ConditionalDiscount : Discount
+    {
+        public Product Product { get; set; }
+        protected ConditionalDiscount(double percentage, DateTime deadline, Product product, Store store) : base(percentage, deadline, store)
+        {
+            Product = product;
+        }
+
+        protected abstract double ApplyDiscount(Basket basket);
+
+
+
+    }
+}
