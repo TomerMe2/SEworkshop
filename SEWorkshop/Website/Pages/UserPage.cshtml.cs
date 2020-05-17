ea2ad9783c77b5ff21f1d6ce39824f0910feb206
neyema <neyema@post.bgu.ac.il>
2020-05-17 13:24:35 +0300

    admin can view user's purchase history

--- SEWorkshop/Website/Pages/UserPage.cshtml.cs
+++ SEWorkshop/Website/Pages/UserPage.cshtml.cs
@@ -16,7 +16,10 @@ namespace Website.Pages
         public IEnumerable<DataPurchase> Purchases { get; private set; }
         public IEnumerable<string> Users { get; set; }
         public bool IsAdmin { get; set; }
-        public string Username { get; set; }
+        public string Username { get; set; }  //current user
+        [BindProperty(SupportsGet = true)]
+        public string SearchUsername { get; set; }
+        public string ErrorMsg { get; set; }
 
         public UserPageModel(IUserManager userManager)
         {
@@ -25,12 +28,15 @@ namespace Website.Pages
             Users = new List<string>();
             IsAdmin = false;
             Username = "";
+            SearchUsername = "";
+            ErrorMsg = "";
         }
 
         public void OnGet(string username)
         {
             string sid = HttpContext.Session.Id;
             Username = (UserManager.GetDataLoggedInUser(sid)).Username;
+            SearchUsername = "";
             IsAdmin = UserManager.IsAdministrator(sid);
             Users = UserManager.GetAllUsers(sid).ToList();
             if (username == null || username.Length == 0)
@@ -44,11 +50,25 @@ namespace Website.Pages
             }
         }
 
+        /*
         public void OnPost(string content, string storeName, string productName)
         {
             string sid = HttpContext.Session.Id;
             UserManager.WriteReview(sid, storeName, productName, content);
             Purchases = UserManager.PurchaseHistory(sid);
+        }
+        */
+        public IActionResult OnPost()
+        {
+            try
+            {
+                Purchases = UserManager.UserPurchaseHistory(HttpContext.Session.Id, SearchUsername);
+            }
+            catch (Exception e)
+            {
+                ErrorMsg = e.ToString();
+            }
+            return new PageResult();
         }
     }
 }
