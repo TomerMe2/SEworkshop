bad831e012a704fd3aa13a58d8bf8eafefb1eb48
lynn <nabulsylynn@gmail.com>
2020-06-21 15:06:42 +0300

    last

--- SEWorkshop/SEWorkshop/Exceptions/IllegalCharactersException.cs
+++ SEWorkshop/SEWorkshop/Exceptions/IllegalCharactersException.cs
@@ -9,6 +9,6 @@
         protected IllegalCharactersException(
             System.Runtime.Serialization.SerializationInfo info,
             System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
-        public override string ToString() => "Username or password contain illegal characters such as: space";
+        public override string ToString() => "Username/password contain illegal characters such as: Space";
     }
 }
