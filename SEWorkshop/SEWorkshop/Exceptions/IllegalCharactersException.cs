97198005dedc73d0c0adb71fda370f74e822999c
lynn <nabulsylynn@gmail.com>
2020-06-21 15:04:06 +0300

    reject usernames containing spaces

---
+++ SEWorkshop/SEWorkshop/Exceptions/IllegalCharactersException.cs
@@ -0,0 +1,14 @@
+﻿namespace SEWorkshop.Exceptions
+{
+    [System.Serializable]
+    public class IllegalCharactersException : TradingSystemException
+    {
+        public IllegalCharactersException() { }
+        public IllegalCharactersException(string message) : base(message) { }
+        public IllegalCharactersException(string message, System.Exception inner) : base(message, inner) { }
+        protected IllegalCharactersException(
+            System.Runtime.Serialization.SerializationInfo info,
+            System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+        public override string ToString() => "Username or password contain illegal characters such as: space";
+    }
+}
