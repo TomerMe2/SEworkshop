af1160e22ba07c4554001117c5e9544ec767cc02
neyema <neyema@post.bgu.ac.il>
2020-06-23 18:24:48 +0300

    I need a function in the backend to get the users by category

--- SEWorkshop/SEWorkshop/ServiceLayer/IUserManager.cs
+++ SEWorkshop/SEWorkshop/ServiceLayer/IUserManager.cs
@@ -75,9 +75,9 @@ namespace SEWorkshop.ServiceLayer
         public void RegisterPurchaseObserver(IServiceObserver<DataPurchase> obsrv);
         public IEnumerable<string> GetAllUsers(string sessionId);
         public double GetIncomeInDate(string sessionId, DateTime date);
-        
+
         //keys are days, values are the number of visitors in the day of the key
-        public IDictionary<DateTime, int> GetUseRecord(string sessionId, DateTime dateFrom, DateTime dateTo, List<SEWorkshop.Enums.KindOfUser> kinds);
+        public IDictionary<DateTime, int> GetUseRecord(string sessionId, DateTime dateFrom, DateTime dateTo, List<KindOfUser> kinds);
         public void AnswerOwnershipRequest(string sessionId, string storeName, string newOwnerUserName, RequestState answer);
         public void RegisterOwnershipObserver(IServiceObserver<DataOwnershipRequest> obsrv);
         public DataUser GetUser(string sessionId);
