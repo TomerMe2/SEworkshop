1a8b099c745d8728fe048fb2a3547d9ef0f88ffc
neyema <neyema@post.bgu.ac.il>
2020-06-24 13:58:07 +0300

    added file

--- SEWorkshop/SEWorkshop/DAL/LocalDbContext.cs
+++ SEWorkshop/SEWorkshop/DAL/LocalDbContext.cs
@@ -7,9 +7,9 @@ namespace SEWorkshop.DAL
 {
     class LocalDbContext : AppDbContext
     {
-        private const string DB_NAME = "Database1.mdf";
+        private const string DB_NAME = "Local.mdf";
         private static readonly string dbPath =
-            "C:\\Users\\amitv\\University\\SEworkshop\\SEWorkshop\\SEWorkshop\\DAL\\Database1.mdf";
+            "C:\\Users\\neyem\\Desktop\\SEworkshop\\SEWorkshop\\SEWorkshop\\DAL\\Local.mdf";
 
         public LocalDbContext() : base(string.Format(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename={0};Integrated Security=True", dbPath))
         {
